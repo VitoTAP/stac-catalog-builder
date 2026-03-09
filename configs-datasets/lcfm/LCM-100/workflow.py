09f35744de3f5b3236f619973b00cf54461e4d65
Joris C <3611895+JorisCod@users.noreply.github.com>
2026-03-09 17:07:36 +0100

    LCFM LCM-100

--- configs-datasets/lcfm/LCM-100/workflow.py
+++ configs-datasets/lcfm/LCM-100/workflow.py
@@ -14,21 +14,22 @@ from stacbuilder import (
 
 # Collection configuration
 product = "LCM-100"
-version = "v005"
-postfix = "_V12C_C232"
+version = "v100"
+target = "LCFM"
+terrascope_collection_id = None
 level = "30deg" if product == "LCM-100" else "3deg"
 catalog_version = "v0.1"
 collection_config_path = Path(__file__).parent.resolve() / "config-collection.json"
 
 # Input Paths
-tiff_input_path = Path(f"/vitodata/vegteam_vol2/products/LCFM/{product}/{version}/tiles_latlon/{level}/")
+tiff_input_path = Path(f"/vitodata/vegteam_lcfm_openeo/products/LCFM/{product}/{version}/tiles_latlon/{level}/")
 print(tiff_input_path)
 assert tiff_input_path.exists(), f"Path does not exist: {tiff_input_path}"
-tiffs_glob = "*/*/2020/*.tif"
+tiffs_glob = "*/*/2020/*_MAP.tif"
 
 # Output Paths
 output_path = Path(__file__).parent.resolve() / "results"
-test_output_path = output_path / "test" / catalog_version
+test_output_path = output_path / target / catalog_version
 publish_output_path = output_path / "publish" / catalog_version
 
 
@@ -48,21 +49,35 @@ for k in asset_metadata:
 
 
 def item_postprocessor(item: pystac.Item) -> pystac.Item:
-    # Proj V2
-    # item.properties["proj:code"] = "EPSG:" + str(item.properties["proj:epsg"])
-    # del item.properties["proj:epsg"]
-    # item.stac_extensions[2] = "https://stac-extensions.github.io/projection/v2.0.0/schema.json"
-    item.id = item.id.replace(postfix, "")
+    if target == "Terrascope":
+        item.collection_id = terrascope_collection_id
 
-    item.properties["tileId"] = item.properties["product_tile"]
-    del item.properties["product_tile"]
+        item.properties["auth:schemes"] = {
+            "oidc": {
+                "type": "openIdConnect",
+                "description": "Authenticate with Terrascope OpenID Connect",
+                "openIdConnectUrl": "https://sso.terrascope.be/auth/realms/terrascope/.well-known/openid-configuration",
+            }
+        }
+        item.stac_extensions.append("https://stac-extensions.github.io/authentication/v1.1.0/schema.json")
+        item.assets["MAP"].extra_fields["auth:refs"] = ["oidc"]
 
-    item.properties["product_version"] = f"{version}"
-
-    # s3_prepend = f"s3://lcfm_waw3-1_4b82fdbbe2580bdfc4f595824922507c0d7cae2541c0799982/vito/products/{product}/{version}/tiles_latlon/{level}/"
-    # s3_prefix = "/".join(item.assets["MAP"].href.split("/")[-4:-1])
-    # s3_name = item.assets["MAP"].href.split("/")[-1].replace(postfix, "")
-    # item.assets["MAP"].href = s3_prepend + s3_prefix + "/" + s3_name
+        item.assets["MAP"].extra_fields["alternate"] = {"local": {"href": "file://" + item.assets["MAP"].href}}
+        item.stac_extensions.append("https://stac-extensions.github.io/alternate-assets/v1.2.0/schema.json")
+        item.assets["MAP"].href = "https://services.terrascope.be/download/" + item.assets["MAP"].href[11:]
+    elif target == "LCFM":
+        # s3_prepend = "s3://lcfm_waw3-1_4b82fdbbe2580bdfc4f595824922507c0d7cae2541c0799982/vito/products/LCM-100/v100/tiles_latlon/30deg/"
+        # s3_prefix = "/".join(item.assets["MAP"].href.split("/")[-4:-1])
+        # s3_name = item.assets["MAP"].href.split("/")[-1].replace("_V12C_C232", "")
+        # item.assets["MAP"].href = s3_prepend + s3_prefix + "/" + s3_name
+        item.assets["MAP"].href = "file://" + item.assets["MAP"].href
+    elif target == "CDSE":
+        s3_prepend = "s3://eodata/CLMS/landcover_landuse/dynamic_land_cover/tcd_pantropical_10m_yearly_v1/2020/01/01/"
+        s3_folder = item.assets["MAP"].href.split("/")[-1].split(".")[0][:-4] + "_cog"
+        s3_name = item.assets["MAP"].href.split("/")[-1]
+        item.assets["MAP"].href = s3_prepend + s3_folder + "/" + s3_name
+    else:
+        raise ValueError(f"Unknown target: {target}")
     return item
 
 
@@ -93,5 +108,5 @@ build_collection(
 
 # validate collection
 validate_collection(
-    collection_file=test_output_path / "collection.json",
+    collection_file=test_output_path / ".." / "collection.json",
 )
